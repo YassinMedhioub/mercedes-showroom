2aff3401450406c36eb447e08097e39894791bf4
Nemss <medhioubyassin6@gmail.com>
2025-08-18 22:20:03 +0100

    changing path in availableCars.vue

--- frontend/mercedes/src/views/AvailableCars.vue
+++ frontend/mercedes/src/views/AvailableCars.vue
@@ -66,8 +66,7 @@ const props = defineProps({
 const emit = defineEmits(['navigate'])
 
 /* Images */
-import aClassAmgLine from '@/assets/cars/AclassAmgLine.png'
-import amgGt from '@/assets/cars/amg_gt.png'
+import { default as aClassAmgLine, default as amgGt } from '@/assets/cars/amg_gt.png'
 import c200 from '@/assets/cars/C200.png'
 import gla200d from '@/assets/cars/GLA200D.png'
 import gleS63 from '@/assets/cars/GLES63.avif'
